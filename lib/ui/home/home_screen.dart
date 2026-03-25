0cc2eb20dc818466e58cd5248638b969024762f2
Abdo <abdoabosheref@gmail.com>
2026-03-25 06:26:55 +0200

    Adding Assets Lib Files

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:evently_app/util/app_style.dart';
+import 'package:flutter/material.dart';
+
+
+
+class HomeScreen extends StatelessWidget {
+   HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(),
+      body: Text('abdo',style: AppStyleLight.reg20,),
+
+
+    );
+
+
+
+
+
+
+  }
+}
---
+++ lib/util/app_color.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+abstract class AppColorLight {
+  static const Color mainColor = Color(0xFF0E3A99);
+  static const Color disable = Color(0xFFB9B9B9);
+  static const Color mainText = Color(0xFF1C1C1C);
+  static const Color secText = Color(0xFF686868);
+  static const Color backGround = Color(0xFFF4F7FF);
+  static const Color inputs = Color(0xFFFFFFFF);
+  static const Color stroke = Color(0xFFF0F0F0);
+  static const Color red = Color(0xFFFF3232);
+  static const Color warning = Color(0xFFF2994A);
+  static const Color success = Color(0xFF2ECC71);
+}
+
+abstract class AppColorDark {
+  static const Color mainColor = Color(0xFF457AED);
+  static const Color disable = Color(0xFFB9B9B9);
+  static const Color mainText = Color(0xFFFFFFFF);
+  static const Color secText = Color(0xFFD6D6D6);
+  static const Color backGround = Color(0xFF000F30);
+  static const Color inputs = Color(0xFF001440);
+  static const Color stroke = Color(0xFF002D8F);
+  static const Color red = Color(0xFFFF3232);
+  static const Color warning = Color(0xFFF2994A);
+  static const Color success = Color(0xFF2ECC71);
+}
