03b6cc8b2207076672b317e54e1d7297b4f84328
Abdo <abdoabosheref@gmail.com>
2026-03-26 03:08:55 +0200

    Adding Assets Lib Files

---
+++ lib/util/app_image.dart
@@ -0,0 +1,8 @@
+abstract class AppImage {
+  static const String forgetPassword = 'assets/images/forget_password.svg';
+  static const String intro1 = 'assets/images/intro1.svg';
+  static const String intro2 = 'assets/images/intro2.svg';
+  static const String intro3 = 'assets/images/intro33.svg';
+  static const String intro4 = 'assets/images/intro4.svg';
+
+}
